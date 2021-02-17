10db0c9986f691979549556da88594f06274234c
Ricardo Münch <munch_141@hotmail.com>
2021-02-17 18:58:24 +0100

    Add super-basic login

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,19 +1,9 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 import './registerServiceWorker';
 import './main.css';
-import HomePage from '@/pages/HomePage.vue';
-import GorilaPage from '@/pages/GorilaPage.vue';
 import App from './App.vue';
 import store from './store';
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: HomePage },
-    { path: '/gorila', component: GorilaPage },
-  ],
-});
+import router from './router';
 
 const app = createApp(App);
 
---
+++ web/src/pages/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container p-6">
+    <h1 class="text-3xl">Login (en construcción)</h1>
+    <button @click="login">login</button>
+    <router-link to="/">home</router-link>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { mapActions } from 'vuex';
+
+export default defineComponent({
+  methods: {
+    ...mapActions(['login']),
+  },
+});
+</script>
