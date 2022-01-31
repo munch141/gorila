6283f62b163ac39a26777d542b5faf70a5010fa5
Ricardo Münch <munch_141@hotmail.com>
2022-01-31 22:30:33 +0100

    Move inventory parser to main page

---
+++ src/Gorila.UI/src/pages/ProductsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="container p-6">
+    <ProductsGrid :enableDelete="true"></ProductsGrid>
+  </section>
+</template>
+
+<script>
+import ProductsGrid from '../components/ProductsGrid.vue';
+
+export default {
+  components: { ProductsGrid },
+};
+</script>
